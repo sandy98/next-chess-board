6ccd9c0c283e6e33b94b1816d02c2ea654277796
Domingo E. Savoretti <esavoretti@gmail.com>
2017-11-22 01:18:29 -0300

    Update README.md

--- pages/old-about.js
+++ pages/old-about.js
@@ -24,7 +24,7 @@ export default class About extends Component {
                 <ChessBoard hideNotation={true} size={160} flipped={true} selectedSqBg="yellow" moveValidator={true} />
         </div>
         <div className="hero">
-          <h3 className="title">React Chess Board v0.4.1</h3>
+          <h3 className="title">React Chess Board v0.4.0</h3>
           <p className="description">
             What are you waiting? Go to <Link href="/"><a>Board Page</a></Link>
           </p>
@@ -37,14 +37,8 @@ export default class About extends Component {
                                        background: '#efefef',
                                        border: 'solid 1px navy',
                                        borderRadius: '10px',
-                                       height: '600em'
                                        }}>
-            <iframe style={{width: '98%', 
-                            border: 'none', 
-                            height: '600em'}} 
-                    src="/static/README.htm"
-                    frameBorder={0} 
-            />
+            <ReactMarkdown source={this.props.readme} />
             <br/><br/>
           </div>
         </div>
